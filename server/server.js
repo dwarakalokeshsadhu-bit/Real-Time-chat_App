94db332aea1b369987e8c5f4095e706b0c0f60d6
dwarakalokeshsadhu-bit <dwarakalokeshsadhu@gmail.com>
2026-05-05 15:33:15 +0530

    Cors

--- server/server.js
+++ server/server.js
@@ -5,11 +5,16 @@ import app from './src/app.js';
 import { connectDB } from './src/config/db.js';
 import { initSocket } from './src/sockets/index.js';
 import { env } from './src/config/env.js';
+import cors from 'cors';
+  
+app.use(cors());
+
 app.use("/uploads", express.static("uploads"));
 await connectDB();
 const httpServer = createServer(app);
 const io = initSocket(httpServer);
 app.set('io', io);
+
 
 httpServer.listen(env.PORT, () => {
   console.log(`Server running on http://localhost:${env.PORT}`);
